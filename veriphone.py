d9cf6ed9667e2df222db612bd004475f13e8278a
Janik <janik@klauenberg.eu>
2020-08-02 15:25:55 +0200

    updated Readme, optimized for python3

--- veriphone.py
+++ veriphone.py
@@ -18,7 +18,7 @@ class Veriphone:
         self.api_key = API_Key
         self.base_url = "https://api.veriphone.io/v2/"
 
-    def Verify_key(self, phone_number):
+    def get_details(self, phone_number):
         """
         Comment: Verifying the API Key for the APi
         Input: Name of Instance
@@ -28,13 +28,14 @@ class Veriphone:
         url = self.base_url + "verify"
         params = {"key" : self.api_key, "phone": phone_number}
         r = requests.get(url, params=params)
-        print r.content
+        return r.content
 
 
 if __name__ == '__main__':
-    import json
+    #Simple If Main for example, will require a creds.json which contains the API Key in a Dict with Key API-Key
+    import json#
     with open("creds.json", "r") as f:
         creds = json.load(f)
     test = Veriphone(creds["API-Key"])
-    inp = raw_input("Please enter a Phone number you want verified: ")
-    test.Verify_key(inp)
+    inp = input("Please enter a Phone number you want verified: ")
+    print (test.get_details(inp))
